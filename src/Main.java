5a29a666f19b99357c7302a22f5abd6684e13f60
nikhil <gogulanikhil@gmail.com>
2023-05-26 16:27:03 +0530

    nikhil created child1 branch

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,9 @@ public class Main {
         System.out.println("Hello suchit!");
         System.out.println("Hi nikhil");
         System.out.println("hi all of you");
+        System.out.println("how");
+        System.out.println("are");
+        System.out.println("you");
         
     }
 }
